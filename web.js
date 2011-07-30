b16de8d2341c4ede73a763a51e5faf4129195333
Refael Ackermann <refack@gmail.com>
2011-07-30 19:52:21 +0300

    Home site

--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/static/*', function(request, response) {
+    console.log("file " + request.params[0]);
+    response.sendfile('static/' + request.params[0]);
+});
+
+app.get('/', function(request, response){
+    response.sendfile('templates/home.html');
 });
 
 var port = process.env.PORT || 3000;

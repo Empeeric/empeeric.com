ac1ac401ffffe2d740b819ddbdc7db7d70ae4f36
Refael Ackermann <refack@gmail.com>
2011-07-30 20:41:17 +0300

    Redirect for sobo

--- web.js
+++ web.js
@@ -3,11 +3,23 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/static/*', function(request, response) {
-    console.log("file " + request.params[0]);
     response.sendfile('static/' + request.params[0]);
 });
 
+app.get('/he/*', function(request, response) {
+    response.redirect('http://sobo.empeeric.com' + request.url, 301);
+});
+app.get('/widget/*', function(request, response) {
+    response.redirect('http://sobo.empeeric.com' + request.url, 301);
+});
+
+app.get('/BrowserSupport.html', function(request, response){
+    response.contentType('text/html');
+    response.sendfile('templates/BrowserSupport.html');
+});
+
 app.get('/', function(request, response){
+    response.contentType('text/html');
     response.sendfile('templates/home.html');
 });
 

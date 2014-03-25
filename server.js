ab7926b4a4edce5df6bf2ad25115580d35c0804d
Refael Ackermann <refael@empeeric.com>
2014-03-25 23:17:36 +0200

    CORS

--- server.js
+++ server.js
@@ -67,6 +67,9 @@ app.get('/snippet/cors/:id', function (req, orig_res) {
 
 
 app.get('/api/kzradio/current', function (req, res) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     kzradio.current(function (show) {
         res.json(show);
     });

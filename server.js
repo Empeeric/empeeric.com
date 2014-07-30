f4ec4fd920cbcb6c5648df6a27f4b1c7b8ffbbc5
Refael Ackermann <refael@empeeric.com>
2014-07-30 16:56:07 +0300

    better GA

--- server.js
+++ server.js
@@ -62,7 +62,8 @@ app.get('/api/formage/checkVer', function* () {
     this.set('Access-Control-Allow-Origin', '*');
     this.set('Access-Control-Allow-Methods', 'GET');
     this.set('Access-Control-Allow-Headers', 'Content-Type');
-    var tracker = ua('UA-15378843-1');
+    var tracker = ua('UA-15378843-1', {headers: this.req.headers, debug:true});
+    tracker.debug(true);
     var clientVer = this.query.version;
     var ref = this.get('referrer') || 'DIRECT';
     var evt = tracker.event("formage", "ping", ref, clientVer);
